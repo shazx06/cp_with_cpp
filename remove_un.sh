1387cb7018a6a2aa7b1efba482cebed6b04288bd
shazib <shazibrahman@outlook.com>
2021-03-15 00:06:38 +0530

    commit

--- remove_un.sh
+++ remove_un.sh
@@ -1,1 +1,7 @@
-find . -type f !  \(   -iname *.cpp -o ! -iname *.txt -o ! -iname *.sh \)
+if $(find . -type f !  \(   -iname *.cpp -o ! -iname *.txt -o ! -iname *.sh \) )
+then
+echo "binary and .o files are removed successfully....."
+
+else
+	echo "$?"
+fi
